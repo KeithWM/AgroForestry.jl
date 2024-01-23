ec6ed10d974e3a3fc1e877a954174bf333ea12e1
KeithWM <keith@myerscough.nl>
2024-01-24 00:07:16 +0100

    Add a background image

--- src/AgroForestry.jl
+++ src/AgroForestry.jl
@@ -4,6 +4,7 @@ import CSV
 using GLMakie
 using Makie.Colors
 using DataFrames
+using FileIO: load
 import Base.convert
 
 export createplot
@@ -12,9 +13,12 @@ include("PlantSpecs.jl")
 
 function loaddata(filepath::AbstractString)
     df = filepath |> CSV.File |> DataFrame
-    return df[1:24, :]
+    return df[1:3, :]
 end
 
+function showname(plant::PlantSpecs.Plant)
+    return plant.name
+end
 function color(m::PlantSpecs.MonthRange)
     return color(m.start, m.finish)
 end
@@ -22,7 +26,7 @@ color(s::Int, f::Int) = Makie.Colors.HSV((s + f) * 15, 0.8, 0.8)
 color(::Missing, ::Missing) = Makie.Colors.HSV(0, 0, 0)
 mean(sr::PlantSpecs.SizeRange) = (sr.start + sr.finish) / 2
 
-function makekwargs(plant::PlantSpecs.Plant)
+function makekwargs(plant::PlantSpecs.Plant, scale::Float64)
     return Dict(
         :color => color(plant.flowering),
         :strokewidth => mean(plant.size.height) / 10,
@@ -31,19 +35,27 @@ function makekwargs(plant::PlantSpecs.Plant)
     )
 end
 
-function createplot(filepath::AbstractString)
+function createplot(filepath::AbstractString, background::AbstractString, scale::Float64)
     df = loaddata(filepath)
     plants = [
         PlantSpecs.Plant(row)
         for row in eachrow(df)
     ]
 
-    dragging = false
-    idx = 1
-    fig, ax, p = scatter(Point{2,Float32}[])
+    # fig, ax, p = scatter(Point{2,Float32}[])
+    fig = Figure()
+    img = load(background)
+    ax, _img = image(
+        fig[1, 1], rotr90(img),
+        axis=(aspect=DataAspect(),)
+    )
+    scale!(_img, scale, scale)
+    limits!(ax, (0, size(img, 2) * scale), (-50, size(img, 1) * scale))
+    ax.xrectzoom = false
+    ax.yrectzoom = false
 
     dms = [
-        plantmarkers(fig, ax, plant, Point2f(0, i))
+        plantmarkers(fig, ax, plant, Point2f(0, i), scale)
         for (i, plant) in enumerate(plants)
     ]
 
@@ -57,25 +69,25 @@ Base.@kwdef mutable struct DraggableMarkers2
     dragging::Bool
 end
 
-function plantmarkers(fig::Figure, ax::Axis, plant::PlantSpecs.Plant, menupoint::Point2f)
+function plantmarkers(fig::Figure, ax::Axis, plant::PlantSpecs.Plant, menupoint::Point2f, scale::Float64)
     positions = Observable([menupoint])
     dm = DraggableMarkers2(
         positions=positions,
         p=scatter!(
-            positions; makekwargs(plant)...
+            positions; markerspace=:data, makekwargs(plant, scale)...
         ),
         idx=0,
         dragging=false,
     )
     t = text!(
         positions;
-        text=plant.name, align=(:center, :center),
+        text=showname(plant), align=(:center, :center), visible=true, fontsize=6,
     )
 
     on(events(fig).mousebutton, priority=2) do event
         if event.button == Mouse.left
             if event.action == Mouse.press
-                plt, i = pick(fig)
+                plt, i = pick(fig, events(fig).mouseposition[], 2)
                 if Keyboard.d in events(fig).keyboardstate && plt == dm.p
                     # Delete marker
                     deleteat!(dm.positions[], i)
